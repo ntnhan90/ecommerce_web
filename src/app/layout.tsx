e45e1b0c1c90b164ad07ba5832c708229caf5787
nguyễn thái nhân <ntnhan90@gmail.com>
2024-05-10 15:13:01 +0700

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@/styles/bootstrap.min.css"
-import "@/styles/font-awesome.min.css"
-import "@/styles/style.min.css"
+//import "@/styles/font-awesome.min.css"
+import "@/styles/style.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
