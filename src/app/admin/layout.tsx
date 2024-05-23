cfe1cb6140f38c72a31ba54e841efd9fb3aa6df1
nguyễn thái nhân <ntnhan90@gmail.com>
2024-05-23 16:44:08 +0700

    update folder public

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import { Container } from 'react-bootstrap'
+import React from 'react'
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center dark:bg-transparent">
+        <>
+            admin layoiut
+        </>
+        <Container>
+            {children}
+        </Container>
+    </div>
+  )
+}
