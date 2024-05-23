c08367700a294c3d0b9e4de1a557001b074d7663
nguyễn thái nhân <ntnhan90@gmail.com>
2024-05-23 23:26:24 +0700

    change structure

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,16 +1,15 @@
 import { Container } from 'react-bootstrap'
 import React from 'react'
 import '@/styles/admin/globals.scss'
+
 export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-        <body>
+    <>
             {children}
-        </body>
-    </html>
+    </>
   )
 }
